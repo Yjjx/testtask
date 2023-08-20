2b9b473e152fed00bbf29c852665724f59057d32
yjjx <rusil.versh@gmail.com>
2023-08-20 15:00:10 +0300

    new task

---
+++ secondtask/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.API.Features;
+using InventorySystem;
+using PlayerRoles;
+
+
+namespace secondtask
+{
+    public class Plugin: Plugin<Config>
+    {
+        public override string Name => "testtask";
+        public override string Author => "yjjx";
+        public override string Prefix => "ftt";
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            Log.Info("secondtask is enabled");
+        }
+    }
+}
---
+++ secondtask/reportlist.cs
@@ -0,0 +1,21 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using System;
+
+namespace secondtask
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    internal class reportlist : ICommand
+    {
+        public string Command => "reportlist";
+
+        public string[] Aliases => new string[] { "rl" };
+        public string Description => "Показывает список репортов";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = null;
+            return true;
+        }
+    }
+}
