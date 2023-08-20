5dbde11661d1fa92c1d1867cb6879d81518e36e3
yjjx <rusil.versh@gmail.com>
2023-08-20 14:58:35 +0300

    Добавьте файлы проекта.

---
+++ firsttasktest/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using Exiled.API.Features;
+using InventorySystem;
+using PlayerRoles;
+
+
+namespace firsttasktest
+{
+    public class Plugin: Plugin<Config>
+    {
+        public override string Name => "testtask";
+        public override string Author => "yjjx";
+        public override string Prefix => "ftt";
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            Log.Info("ftt is enabled");
+            Exiled.Events.Handlers.Player.Spawned += Player_Spawned;
+            Exiled.Events.Handlers.Warhead.Detonated += Warhead_Detonated;
+        }
+
+        private void Warhead_Detonated()
+        {
+            foreach (Exiled.API.Features.Player Player in Exiled.API.Features.Player.List) 
+            {
+                Player.Kill("Вы были уничтожены боеголовкой Омега"); //просто убивает всех.
+            }
+        }
+
+        private void Player_Spawned(Exiled.Events.EventArgs.Player.SpawnedEventArgs ev)
+        {
+            if (ev.Player.Role.Type is RoleTypeId.Scientist) //проверяет что роль игрока учёный.
+            {
+                ev.Player.Health = 500;
+                ev.Player.Inventory.ServerAddItem(ItemType.SCP500);
+                ev.Player.Inventory.ServerAddAmmo(ItemType.Ammo9x19, 100);
+                /*
+                 * ну и выдаём нужные вещи:
+                 * 500 хп.
+                 * scp 500.
+                 * 100 патронов 9x19.
+                 */
+            }
+        }
+    }
+}
---
+++ firsttasktest/changenick.cs
@@ -0,0 +1,32 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using System;
+
+namespace ClassLibrary1
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    internal class changenick : ICommand
+    {
+        public string Command => "changenick";
+
+        public string[] Aliases => new string[] { "cn" };
+        public string Description => "Меняет ваш ник";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            string nick = string.Join(" ", arguments); //переводим аргументы в строку 
+            if (string.IsNullOrWhiteSpace(nick)) //проверяем что игрок ввёл хоть что-то
+            {
+                response = "Введите ник, .cn NICKNAME";
+                return false;
+            }
+            else
+            {
+                var PlayerGet = Player.Get(sender);
+                PlayerGet.DisplayNickname = nick; //присваеваем имя
+                response = "Успешно";
+                return true;
+            }
+        }
+    }
+}
