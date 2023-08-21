9a65fac904b2772707551c1f0d8377e13c7f1b05
yjjx <rusil.versh@gmail.com>
2023-08-21 20:30:59 +0300

    add tesla

--- secondtask/Config.cs
+++ secondtask/Config.cs
@@ -1,14 +1,14 @@
-﻿using Exiled.API.Interfaces;
+﻿using Exiled.API.Features.Items;
+using Exiled.API.Interfaces;
+using Exiled.Events.Handlers;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace secondtask
 {
     public class Config : IConfig
     {
+        public List<Exiled.API.Features.Items.Item> Items { get; set; } = new List<Exiled.API.Features.Items.Item>((int)ItemType.KeycardO5);
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
     }
