a3a99d49572e5f9dc01bfdee4564e4bbc8efd41d
yjjx <rusil.versh@gmail.com>
2023-08-23 13:57:56 +0300

    fast fix

--- secondtask/Delete.cs
+++ secondtask/Delete.cs
@@ -16,14 +16,14 @@ namespace secondtask
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
             var PlayerGet = Exiled.API.Features.Player.Get(sender);
-            if (float.TryParse(string.Join("", arguments), out float dis))
+            if (float.TryParse(string.Join("", arguments), out float radius))
             {
                 Vector3 position = PlayerGet.Position;
                 int rc = 0;
                 int pc = 0;
                 foreach (var doll in Ragdoll.List)
                 {
-                    if (Vector3.Distance(doll.Position, position) <= dis)
+                    if (Vector3.Distance(doll.Position, position) <= radius)
                     {
                         doll.Destroy();
                         rc++;
@@ -31,13 +31,13 @@ namespace secondtask
                 }
                 foreach (var item in Pickup.List)
                 {
-                    if (Vector3.Distance(item.Position, position) <= dis)
+                    if (Vector3.Distance(item.Position, position) <= radius)
                     {
                         item.Destroy();
                         pc++;
                     }
                 }
-                response = $"Удалено {rc} трупов и {pc} предметов в {dis} радиусе";
+                response = $"Удалено {rc} трупов и {pc} предметов в {radius} радиусе";
                 return true;
             }
             else
